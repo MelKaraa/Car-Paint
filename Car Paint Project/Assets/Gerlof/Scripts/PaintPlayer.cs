8821634b683c280e564212cd66c8a1ff8bd230a8
Gerrielofo <gerlof.reinink@gmail.com>
2023-02-21 11:38:52 +0100

    Paintgun ammo

--- Car Paint Project/Assets/Gerlof/Scripts/PaintPlayer.cs
+++ Car Paint Project/Assets/Gerlof/Scripts/PaintPlayer.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PaintPlayer : MonoBehaviour
 {
     [SerializeField] ParticleSystem paintParticle;
-
+    [SerializeField] float ammoAddMultiplier;
+    [SerializeField] float ammoUseMultiplier;
     bool spray;
+    float paintAmmo = 50;
+
+    [SerializeField] TMP_Text ammoTxt;
+
     public void Spray()
     {
         spray = !spray;
@@ -14,13 +20,25 @@ public class PaintPlayer : MonoBehaviour
 
     private void Update()
     {
-        if(spray)
+        if (paintAmmo >= 100)
+        {
+            paintAmmo = 100;
+            ammoTxt.text = "Paint: " + paintAmmo.ToString("F0");
+            return;
+        }
+        else
+        {
+            paintAmmo += Time.deltaTime * ammoAddMultiplier;
+        }
+        if (spray && paintAmmo > 0)
         {
             paintParticle.Play();
+            paintAmmo -= Time.deltaTime * ammoUseMultiplier;
         }
         else
         {
             paintParticle.Stop();
         }
+        ammoTxt.text = "Paint: " + paintAmmo.ToString("F0");
     }
 }
