ad0058b83893a58e134f4f04c4e387e246de2e37
Gerrielofo <gerlof.reinink@gmail.com>
2023-03-28 17:00:46 +0200

    Nee

--- Car Paint Project/Assets/Gerlof/Scripts/WaypointSystem.cs
+++ Car Paint Project/Assets/Gerlof/Scripts/WaypointSystem.cs
@@ -1,47 +1,84 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class WaypointSystem : MonoBehaviour
 {
     private Transform target;
-    private int waypointindex = 0;
+    [SerializeField] private int waypointindex = 0;
 
     [SerializeField] private float movementSpeed;
-   
+    [SerializeField] private Animator door1;
+    [SerializeField] private Animator door2;
+    [SerializeField] bool end;
+    [SerializeField] bool shopEntered;
 
     void Start()
     {
         target = Waypoints.points[0];
+        end = false;
     }
 
     void Update()
-    {      
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * movementSpeed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+    {   
+        if(!end)
         {
-            GetNextWaypoint();
+            Vector3 dir = target.position - transform.position;
+            transform.Translate(dir.normalized * movementSpeed * Time.deltaTime, Space.World);
         }
         
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            if(waypointindex == 0)
+            {
+                door1.SetTrigger("DoorMove");
+            }
+            else if (waypointindex == 3 && !shopEntered)
+            {
+                InShop();
+            }
+            else if(waypointindex == 4 && shopEntered)
+            {
+                door2.SetTrigger("DoorMove");
+            }
+
+            GetNextWaypoint();
+        }
+
     }
 
     void GetNextWaypoint()
     {
         if (waypointindex >= Waypoints.points.Length - 1)
         {
-            EndPath();
-            return;
+            ExitShop();
+            end = true;
         }
-
+        if (end)
+            return;
         waypointindex++;
+   
         target = Waypoints.points[waypointindex];
+
         transform.LookAt(target);
     }
 
-    void EndPath()
+    void InShop()
     {
-        Debug.Log("Car has arrived");
+        if(!shopEntered) 
+        {
+            door1.SetTrigger("DoorMove");
+            Debug.Log("Car has arrived");
+            shopEntered = true;
+            end = true;
+        }
+        
+    }
+
+    void ExitShop()
+    {
+        if(!end)
+        door2.SetTrigger("DoorMove");
     }
 
 }
