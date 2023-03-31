bd5240b6fd0cadf6930f17d3fee9a2af194f99a1
Gerrielofo <gerlof.reinink@gmail.com>
2023-03-31 13:10:39 +0200

    MenuBackground + trigger anim

---
+++ Car Paint Project/Assets/Gerlof/Scripts/SprayTriggerAnim.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class SprayTriggerAnim : MonoBehaviour
+{
+    [SerializeField] InputActionProperty trigger;
+    [SerializeField] Animator triggerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = trigger.action.ReadValue<float>();
+        triggerAnimator.SetFloat("Trigger", triggerValue);
+    }
+}
